3b772b8eebf0f25977fda3f38cfc807d160327d8
Vít Starý Novotný <witiko@mail.muni.cz>
2024-07-10 12:53:25 +0200

    Add `fixup-languages` subcommand to `template.py`

--- template.py
+++ template.py
@@ -6,10 +6,16 @@ Processes ISTQB documents written with the LaTeX+Markdown template.
 """
 
 from argparse import ArgumentParser, Namespace
+from configparser import ConfigParser
+import json
 import logging
 from pathlib import Path
 import re
+import subprocess
+from typing import Iterable
 import os
+
+import yaml
 
 
 LOGGER = logging.getLogger(__name__)
@@ -19,13 +25,7 @@ DOCUMENT_FILETYPES = sorted(['xlsx', 'markdown', 'eps', 'tex', 'bib'])
 FILETYPES = METADATA_FILETYPES + DOCUMENT_FILETYPES
 
 
-def find_files(args: Namespace):
-    paths = sorted(_find_files(args.filetype))
-    for path in paths:
-        print(path)
-
-
-def _find_files(file_type: str, root=Path('.')):
+def _find_files(file_type: str, root=Path('.')) -> Iterable[Path]:
     for parent_directory, subdirectories, filenames in os.walk(root, topdown=True, onerror=print, followlinks=True):
 
         def keep_filename(filename: str):
@@ -91,6 +91,54 @@ def _find_files(file_type: str, root=Path('.')):
                 yield Path(parent_directory) / filename
 
 
+def _fixup_language(path: Path) -> None:
+    # Is `babel-language` already in the language definitions?
+    with path.open('rt') as rf:
+        input_yaml_text = rf.read()
+    input_yaml = yaml.safe_load(input_yaml_text)
+    if 'babel-language' in input_yaml:
+        LOGGER.debug(f'File %s already contains `babel-language`', path)
+        return
+
+    # Determine the babel name of the language.
+    iso_code = path.name[:2]
+    pathname = f'{iso_code}/babel-{iso_code}.ini'
+    config_filenames = subprocess.check_output(['kpsewhich', pathname], text=True).splitlines()
+    assert len(config_filenames) <= 1
+    if len(config_filenames) != 1:
+        raise ValueError(f'File "{pathname}" not found in your TeX installation (is TeX installed?)')
+    config_filename, = config_filenames
+
+    config = ConfigParser()
+    config.read(config_filename)
+    if 'identification' not in config:
+        raise ValueError(f'Section "identification" not found in file "{config_filename}"')
+    if 'name.babel' not in config['identification']:
+        raise ValueError(f'Field "name.babel" not found in section "identification" of file "{config_filename}"')
+    babel_names = config['identification']['name.babel'].split()
+    assert len(babel_names) > 0
+    babel_name, *_ = babel_names
+    if len(babel_names) > 1:
+        LOGGER.info('Found multiple babel names in file "%s": %s; using the first one: %s', config_filename, ', '.join(babel_names), babel_name)
+
+    # Add `babel-language` on top of the language definitions.
+    LOGGER.info('Adding "babel-language: %s" to file "%s"', babel_name, path)
+    with path.open('wt') as wf:
+        print(f'babel-language: {json.dumps(babel_name)}', file=wf)
+        wf.write(input_yaml_text)
+
+
+def find_files(args: Namespace) -> None:
+    paths = sorted(_find_files(args.filetype))
+    for path in paths:
+        print(path)
+
+
+def fixup_languages(args: Namespace) -> None:
+    for path in _find_files(file_type='languages'):
+        _fixup_language(path)
+
+
 def main():
     parser = ArgumentParser(prog='IstqbTemplate', description='Process ISTQB documents written with the LaTeX+Markdown template')
     subparsers = parser.add_subparsers()
@@ -98,6 +146,9 @@ def main():
     parser_find = subparsers.add_parser('find-files', help='Produce a newline-separated list of different types of files in this repository')
     parser_find.add_argument('filetype', choices=FILETYPES, default='all')
     parser_find.set_defaults(func=find_files)
+
+    parser_fixup_languages = subparsers.add_parser('fixup-languages', help='Determine and add `babel-language` to language definitions if missing')
+    parser_fixup_languages.set_defaults(func=fixup_languages)
 
     args = parser.parse_args()
     if not 'func' in args:
