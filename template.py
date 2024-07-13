fa268eedf8386afbcf2082ef56588e559f32c118
Vít Starý Novotný <witiko@mail.muni.cz>
2024-07-13 14:14:51 +0200

    Add logging level to messages

--- template.py
+++ template.py
@@ -711,5 +711,5 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
     main()
