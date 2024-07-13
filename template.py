4c7ff683a58a905f1ded98c4353191a2b0b0ce37
Vít Starý Novotný <witiko@mail.muni.cz>
2024-07-13 15:06:42 +0200

    Update logging format

--- template.py
+++ template.py
@@ -756,5 +756,5 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s\t[%(levelname)s]\t%(message)s')
     main()
